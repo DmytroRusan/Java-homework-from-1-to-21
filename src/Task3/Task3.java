ced189550e1308908c42ec42e32d2e3b8ac5630a
Dmytro <rusandmytro@gmail.com>
2022-09-27 12:05:44 +0300

    Task3

---
+++ src/Task3/Task3.java
@@ -0,0 +1,12 @@
+package Task3;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть будь-яке дробове число: ");
+        double num = scanner.nextDouble();
+        System.out.println((int) num);
+    }
+}
