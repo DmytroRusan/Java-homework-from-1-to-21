0fcfb477d3c9d262dc962fac81545657e9454929
Dmytro <rusandmytro@gmail.com>
2022-10-13 09:26:47 +0300

    Task20

---
+++ src/Task20/Task20.java
@@ -0,0 +1,31 @@
+package Task20;
+
+import java.util.Arrays;
+
+public class Task20 {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 20 + 10);
+        }
+        System.out.println(Arrays.toString(array));
+
+        boolean isGrowing = false;
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) { //обмеження, щоб не вийти за межі масиву
+                break;
+            }
+            if (array[i] < array[i + 1]) {
+                isGrowing = true;
+            } else {
+                isGrowing = false;
+                break;
+            }
+        }
+        if (isGrowing) {
+            System.out.println("Масив є строго зростаючим");
+        } else {
+            System.out.println("Масив не є строго зростаючим");
+        }
+    }
+}
